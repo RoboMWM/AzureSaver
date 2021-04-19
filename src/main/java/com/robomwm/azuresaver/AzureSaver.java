e0858e47e7c13f77be53c0dea8fce7e72f261373
Nick Eidler <nickeidler@gmail.com>
2021-04-18 20:03:58 -0700

    Add support for minutesUntilShutdown option, added some more logging

--- src/main/java/com/robomwm/azuresaver/AzureSaver.java
+++ src/main/java/com/robomwm/azuresaver/AzureSaver.java
@@ -1,16 +1,13 @@
 package com.robomwm.azuresaver;
 
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.PowerState;
 import com.microsoft.azure.management.compute.VirtualMachine;
 import com.microsoft.rest.LogLevel;
 import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.api.event.ServerConnectEvent;
-import net.md_5.bungee.api.event.ServerDisconnectEvent;
-import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
@@ -32,9 +29,12 @@ import java.util.concurrent.TimeUnit;
  */
 public class AzureSaver extends Plugin implements Listener
 {
+    private final int DEFAULT_MINUTES_UNTIL_SHUTDOWN = 10;
+
     private Azure azure;
     private VirtualMachine vm;
     private ScheduledTask task;
+    private int minutesUntilShutdown;
 
     @Override
     public void onEnable()
@@ -63,6 +63,7 @@ public class AzureSaver extends Plugin implements Listener
             }
             Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
             vm = azure.virtualMachines().getByResourceGroup(configuration.getString("resourceGroup"), configuration.getString("vmName"));
+            minutesUntilShutdown = configuration.getInt("minutesUntilShutdown", DEFAULT_MINUTES_UNTIL_SHUTDOWN);
         }
         catch (IOException e)
         {
@@ -85,49 +86,61 @@ public class AzureSaver extends Plugin implements Listener
     @EventHandler
     public void onJoin(PostLoginEvent event)
     {
-        if (this.getProxy().getPlayers().size() > 1)
+        // Whenever somebody joins, reset any shutdown task that may be running
+        if (task != null) {
+            getProxy().getScheduler().cancel(task);
+        }
+
+        getLogger().info("VM PowerState: " + vm.powerState());
+
+        // No-op if the server is already running
+        if (vm.powerState().equals(PowerState.RUNNING)) {
             return;
+        }
+
+        // Start the server (synchronous)
+        getLogger().info("Starting VM...");
         vm.start();
+        getLogger().info("Started VM!");
+
         ServerInfo server = this.getProxy().getServerInfo("lobby");
         event.getPlayer().connect(server, new Callback<Boolean>()
         {
             @Override
             public void done(Boolean result, Throwable error)
             {
-                getLogger().info("k done");
+                getLogger().info("Connected player to lobby server.");
             }
         }, true, 25);
     }
 
     @EventHandler
-    public void onJoinServer(LoginEvent event)
-    {
-        getLogger().info("javadocs succ");
-        if (event.isCancelled())
-            event.setCancelReason("ok boomer");
-    }
-
-    @EventHandler
     public void onLastLeave(PlayerDisconnectEvent event)
     {
-        getLogger().info(String.valueOf(this.getProxy().getPlayers().size()));
-
-        if (this.getProxy().getPlayers().size() > 1) //player is removed from list after this event
+        // No-op if there are still people left on the server
+        if (this.getProxy().getPlayers().size() > 1) { //player is removed from list after this event
             return;
+        }
 
-        getLogger().info("scheduling");
+        getLogger().info("Last player has disconnected. Scheduling shutdown in " + minutesUntilShutdown + " minutes...");
 
-        if (task != null)
+        // Cancel task if there is an earlier one in-flight
+        if (task != null) {
             getProxy().getScheduler().cancel(task);
+        }
 
+        // Schedule task to shut down VM
         task = getProxy().getScheduler().schedule(this, () ->
         {
             task = null;
-            if (getProxy().getPlayers().size() > 0)
+            if (getProxy().getPlayers().size() > 0) {
                 return;
-            //turn off VM
-            getLogger().info("turning off");
+            }
+
+            // Deallocate off VM
+            getLogger().info("Deallocating VM...");
             vm.deallocate();
-        }, 10, TimeUnit.MINUTES);
+            getLogger().info("Deallocated VM!");
+        }, minutesUntilShutdown, TimeUnit.MINUTES);
     }
 }
